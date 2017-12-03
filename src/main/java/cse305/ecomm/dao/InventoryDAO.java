e0e7a8cc21b68d3ee3432b9ce7dd3bda2ae4de71
shreelock <shreenath.dutt@gmail.com>
2017-12-02 20:36:36 -0500

    activeinvetory get

--- src/main/java/cse305/ecomm/dao/InventoryDAO.java
+++ src/main/java/cse305/ecomm/dao/InventoryDAO.java
@@ -1,8 +1,6 @@
 package cse305.ecomm.dao;
 
-import cse305.ecomm.representations.Address;
 import cse305.ecomm.representations.InventoryQtyResponse;
-import cse305.ecomm.representations.Item;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,29 +18,14 @@ public class InventoryDAO {
         Class.forName("com.mysql.jdbc.Driver");
         connect = DriverManager.getConnection("jdbc:mysql://a.vshukla.in:3306/?user=admin&password=password");
         statement = connect.createStatement();
-        String query = "SELECT item.* , sum(quantity) FROM amazeon.inventory JOIN amazeon.item where inventory.item_id = item.item_id AND quantity>0 group by item.item_id;";
+        String query = "SELECT item.item_id, item.item_name , sum(quantity) FROM amazeon.inventory JOIN amazeon.item where inventory.item_id = item.item_id AND quantity>0 group by item.item_id;";
         ResultSet rs = statement.executeQuery(query);
 
         List<InventoryQtyResponse> itemQtyRespList = new ArrayList<>();
         while(rs.next()) {
-            Item item = new Item(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getFloat(4), rs.getString(5), rs.getString(6));
-            itemQtyRespList.add(new InventoryQtyResponse(item, rs.getInt(7)));
+            itemQtyRespList.add(new InventoryQtyResponse(rs.getInt(1), rs.getString(2), rs.getInt(3)));
         }
         return itemQtyRespList;
     }
 
-    public Address getAddrFromPersonId(int personId) throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
-        connect = DriverManager.getConnection("jdbc:mysql://a.vshukla.in:3306/?user=admin&password=password");
-        statement = connect.createStatement();
-
-        String query = "SELECT * FROM amazeon.address where person_id = " + personId + ";";
-
-        ResultSet res = statement.executeQuery(query);
-        if(res.next()) {
-            System.out.println(res);
-            return new Address(res.getInt(1), res.getString(3), res.getString(2));
-        }
-        return new Address(0,"NULL", "NULLTyPE");
-    }
 }
--- src/main/java/cse305/ecomm/representations/InventoryQtyResponse.java
+++ src/main/java/cse305/ecomm/representations/InventoryQtyResponse.java
@@ -4,27 +4,40 @@ import javax.validation.constraints.NotNull;
 
 public class InventoryQtyResponse {
     @NotNull
-    private Item item;
+    private Integer itemId;
+    @NotNull
+    private String itemName;
     @NotNull
     private Integer quantity=0;
 
-    public InventoryQtyResponse(Item item, int quantity) {
-        this.item = item;
+    public InventoryQtyResponse(int itemId, String itemName, int quantity) {
+        this.itemId = itemId;
+        this.itemName = itemName;
         this.quantity = quantity;
     }
 
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public Item getItem() { return this.item; }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public int getQuantity() {
+    public Integer getQuantity() {
         return quantity;
     }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
 }
